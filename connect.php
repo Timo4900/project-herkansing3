1e7dc365530dd725ff573d79f7772c375811f84c
Timo Hofstede <timohofstede49@gmail.com>
2026-05-19 20:34:51 +0200

    Toevoeging: connectie naar db gemaakt.

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "planner";
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+?>
